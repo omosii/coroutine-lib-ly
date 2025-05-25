43bbf28bb4e5f286852c9b4f1d66419bea968a13
omosii <1584303256@qq.com>
2025-05-25 23:32:59 +0800

    semaphore_complete

--- coroutine-lib-ly/fiber_lib/1thread/thread_ly.h
+++ coroutine-lib-ly/fiber_lib/1thread/thread_ly.h
@@ -8,12 +8,39 @@
 namespace sylar
 {
 
-    // 用于线程方法间的同步
-    class Semaphore
+// 用于线程方法间的同步
+class Semaphore
+{
+private:
+    std::mutex mtx;
+    std::condition_variable cv;
+    int count;
+public:
+    // 信号量初始化为0
+    explicit Semaphore(int count_ = 0) : count(count_){}
+
+    // p
+    void wait()
     {
-    private:
-        std::mutex mtx;
-    };
+        std::unique_lock<std::mutex> lock(mtx);
+        while(count == 0) // 必须用while,避免虚假唤醒与多线程竞争
+        {
+            cv.wait(lock);
+        }
+        count--;
+    }
+
+    // v
+    void signal()
+    {
+        std::unique_lock<std::mutex> lock(mtx);
+        count++;
+        cv.notify_one();
+    }
+
+};
+
+
 }
 
 #endif
