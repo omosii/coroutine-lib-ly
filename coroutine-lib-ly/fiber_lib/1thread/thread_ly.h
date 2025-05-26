a1dded1da65bdf99bf02f5581d98ef69cd51c665
omosii <1584303256@qq.com>
2025-05-26 12:17:33 +0800

    thread_ly complete

--- coroutine-lib-ly/fiber_lib/1thread/thread_ly.h
+++ coroutine-lib-ly/fiber_lib/1thread/thread_ly.h
@@ -40,6 +40,57 @@ public:
 
 };
 
+// 一共两种线程: 1 由系统自动创建的主线程 2 由Thread类创建的线程
+class Thread
+{
+public:
+    Thread(std::function<void()> cb, const std::string& name);
+    ~Thread();
+
+    pid_t getId() const {return m_id;}
+    const std::string& getName() const {return m_name;}
+
+    void join();
+
+public:
+    // 获取操作系统分配的线程id，编号
+    static pid_t GetThreadId();
+    // 获取当前所在线程
+    static Thread* GetThis();
+
+    // 获取当前线程的名字
+    static const std::string& GetName();
+    // 设置当前线程的名字
+    static void SetName(const std::string& name);
+
+private:
+    // 线程函数
+    static void* run(void* arg);
+
+private:
+    pid_t m_id = -1;
+    pthread_t m_thread = 0;
+
+    // 线程需要运行的函数
+    std::function<void()> m_cb;
+    std::string m_name;
+
+    Semaphore m_semaphore;
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
 
