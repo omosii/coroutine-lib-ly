4a9f7d660dd902ac40540b4b0249e682e32b7e66
omosii <1584303256@qq.com>
2025-06-02 23:49:07 +0800

    timer_ly_0602_234800

--- coroutine-lib-ly/fiber_lib/4timer/timer_ly.cpp
+++ coroutine-lib-ly/fiber_lib/4timer/timer_ly.cpp
@@ -46,7 +46,36 @@ bool Timer::refresh()
     return true;
 }
 
+bool Timer::reset(uint64_t ms, bool from_now)
+{
+    if(ms == m_ms && from_now == false)
+    {
+        return true;
+    }
 
+    {
+        std::unique_lock<std::shared_mutex> write_lock(m_manager->m_mutex);
+        
+        if(!m_cb)
+        {
+            return false;
+        }
+        
+        auto it = m_manager->m_timers.find(shared_from_this());
+        if(it == m_manager->m_timers.end())
+        {
+            return false;
+        }
+        m_manager->m_timers.erase(it);
+    }
+    // reinsert
+    auto start = from_now ? std::chrono::system_clock::now() : m_next - std::chrono::milliseconds(m_ms);
+    m_next = start + std::chrono::milliseconds(ms);
+    m_ms = ms;
+    m_manager->addTimer(shared_from_this()); // insert with lock
+
+    return true;
+}
 
 
 
