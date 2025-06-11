37ae475a33dd4a5563bb31ff8e6a4312b7c95f73
omosii <1584303256@qq.com>
2025-06-11 20:50:02 +0800

    hook begin

---
+++ coroutine-lib-ly/fiber_lib/6hook/scheduler_ly.h
@@ -0,0 +1,153 @@
+#ifndef _SCHEDULER_LY_H_
+#define _SCHEDULER_LY_H_
+
+#include "thread_ly.h"
+#include "fiber_ly.h"
+
+#include <mutex>
+#include <vector>
+
+namespace sylar{
+
+class Scheduler
+{
+public:
+    // 线程数量 是否将主线程作为工作线程 调度器名称
+    Scheduler(size_t threads = 1, bool use_caller = true, const std::string& name = "Scheduler");
+    virtual ~Scheduler();
+
+    const std::string& getName() const {return m_name;}
+
+public:
+    // 获取正在运行的调度器
+    static Scheduler* GetThis();
+
+protected:
+    // 设置正在运行的调度器
+    void SetThis();
+
+public:
+    // 添加任务到任务队列 FiberOrCb是调度任务类型，可以是协程或者函数
+    template <class FiberOrCb>
+    void scheduleLock(FiberOrCb fc, int thread = -1)
+    {   
+        bool need_tickle;// 用于标记任务队列是否为空，从而判断是否要唤醒线程
+        {
+            std::lock_guard<std::mutex> lock(m_mutex);
+            // empty ->  all thread is idle -> need to be waken up
+            need_tickle = m_tasks.empty();
+
+            ScheduleTask task(fc, thread);// 创建任务对象
+            if(task.fiber || task.cb)       
+            {
+                m_tasks.push_back(task);
+            }     
+        }
+
+        if(need_tickle) // 若队列为空唤醒线程 
+        {
+            tickle();
+        }
+    }
+
+	// 启动线程池 
+    virtual void start();
+    // 关闭线程池
+    virtual void stop();
+
+protected:
+    // 唤醒线程
+    virtual void tickle();
+
+    // 线程函数
+    virtual void run();
+
+    // 空闲协程函数， 无任务时，执行idle协程
+    virtual void idle();
+
+    // 是否可以关闭
+    virtual bool stopping();
+
+    // 返回是否有空闲线程
+    // 具体来说，当调度协程进入idle时，空闲线程数+1；从idle协程返回时，空闲线程数-1
+    bool hasIdleThreads() {return m_idleThreadCount > 0;}
+
+private:
+    // 任务
+    struct ScheduleTask  
+    {
+        std::shared_ptr<Fiber> fiber;
+        std::function<void()> cb;
+        int thread; // 指定任务需要运行的线程id
+
+        ScheduleTask()
+        {
+            fiber = nullptr;
+            cb = nullptr;
+            thread = -1;
+        }
+
+        ScheduleTask(std::shared_ptr<Fiber> f, int thr)
+        {
+            fiber = f;
+            thread = thr;
+        }
+
+        ScheduleTask(std::shared_ptr<Fiber>* f, int thr)
+        {
+            // f 是一个指向 std::shared_ptr<Fiber> 的指针，即 shared_ptr 的指针。
+            fiber.swap(*f); // 通过 swap 转移所有权
+            thread = thr;
+        }
+
+        ScheduleTask(std::function<void()> f, int thr)
+        {
+            cb = f;
+            thread = thr;
+        }
+
+        ScheduleTask(std::function<void()> *f, int thr)
+        {
+            cb.swap(*f);
+            thread = thr;
+        }
+
+        void reset()
+        {
+            fiber = nullptr;
+            cb = nullptr;
+            thread = -1;
+        }
+    };
+private:
+    std::string m_name;
+    // 互斥锁 -> 保护任务队列
+	std::mutex m_mutex;
+    // 线程池
+    std::vector<std::shared_ptr<Thread>> m_threads;
+    // 任务队列
+    std::vector<ScheduleTask> m_tasks;
+    // 存储工作线程的线程id
+    std::vector<int> m_threadIds;
+    // 需要额外创建的线程数
+    size_t m_threadCount = 0;
+    // 活跃线程数
+    std::atomic<size_t> m_activeThreadCount = {0}; // 赋值号= 可省略
+    // 空闲线程数
+    std::atomic<size_t> m_idleThreadCount = {0};
+
+    // 主线程是否用作工作线程
+    bool m_useCaller;
+    // 如果是 -> 需要额外创建调度协程
+    std::shared_ptr<Fiber> m_schedulerFiber;
+    // 如果是 -> 记录主线程的线程id
+    int m_rootThread = -1;
+    // 是否正在关闭
+    bool m_stopping = false;
+};
+
+
+}
+
+
+#endif
--- coroutine-lib-ly/fiber_lib/6hook/timer_ly.cpp
+++ coroutine-lib-ly/fiber_lib/6hook/timer_ly.cpp
@@ -1,83 +1,84 @@
-#include "timer.h"
+#include "timer_ly.h"
 
-namespace sylar {
+namespace sylar{
 
-bool Timer::cancel() 
+bool Timer::cancel()
 {
     std::unique_lock<std::shared_mutex> write_lock(m_manager->m_mutex);
 
-    if(m_cb == nullptr) 
+    if(m_cb == nullptr)
     {
         return false;
     }
-    else
-    {
+    else{
         m_cb = nullptr;
     }
 
     auto it = m_manager->m_timers.find(shared_from_this());
-    if(it!=m_manager->m_timers.end())
+    if(it != m_manager->m_timers.end())
     {
         m_manager->m_timers.erase(it);
     }
+
     return true;
 }
 
-// refresh 只会向后调整
-bool Timer::refresh() 
+// refresh 只会向后调整时间
+bool Timer::refresh()
 {
     std::unique_lock<std::shared_mutex> write_lock(m_manager->m_mutex);
 
-    if(!m_cb) 
-    {
+    if(!m_cb){
         return false;
     }
 
     auto it = m_manager->m_timers.find(shared_from_this());
-    if(it==m_manager->m_timers.end())
+    if(m_manager->m_timers.end() == it)
     {
         return false;
     }
 
     m_manager->m_timers.erase(it);
-    m_next = std::chrono::system_clock::now() + std::chrono::milliseconds(m_ms);
+    m_next = std::chrono::system_clock::now() + 
+                std::chrono::milliseconds(m_ms);
     m_manager->m_timers.insert(shared_from_this());
+
     return true;
 }
 
-bool Timer::reset(uint64_t ms, bool from_now) 
+bool Timer::reset(uint64_t ms, bool from_now)
 {
-    if(ms==m_ms && !from_now)
+    if(ms == m_ms && from_now == false)
     {
         return true;
     }
 
     {
         std::unique_lock<std::shared_mutex> write_lock(m_manager->m_mutex);
-    
-        if(!m_cb) 
+        
+        if(!m_cb)
         {
             return false;
         }
         
         auto it = m_manager->m_timers.find(shared_from_this());
-        if(it==m_manager->m_timers.end())
+        if(it == m_manager->m_timers.end())
         {
             return false;
-        }   
-        m_manager->m_timers.erase(it); 
+        }
+        m_manager->m_timers.erase(it);
     }
-
     // reinsert
     auto start = from_now ? std::chrono::system_clock::now() : m_next - std::chrono::milliseconds(m_ms);
+    m_next = start + std::chrono::milliseconds(ms);
     m_ms = ms;
-    m_next = start + std::chrono::milliseconds(m_ms);
     m_manager->addTimer(shared_from_this()); // insert with lock
+
     return true;
 }
 
 Timer::Timer(uint64_t ms, std::function<void()> cb, bool recurring, TimerManager* manager):
-m_recurring(recurring), m_ms(ms), m_cb(cb), m_manager(manager) 
+m_recurring(recurring), m_ms(ms), m_cb(cb), m_manager(manager)
 {
     auto now = std::chrono::system_clock::now();
     m_next = now + std::chrono::milliseconds(m_ms);
@@ -85,23 +86,25 @@ m_recurring(recurring), m_ms(ms), m_cb(cb), m_manager(manager)
 
 bool Timer::Comparator::operator()(const std::shared_ptr<Timer>& lhs, const std::shared_ptr<Timer>& rhs) const
 {
-    assert(lhs!=nullptr&&rhs!=nullptr);
+    assert(lhs!=nullptr && rhs!=nullptr);
     return lhs->m_next < rhs->m_next;
 }
 
-TimerManager::TimerManager() 
+TimerManager::TimerManager()
 {
     m_previouseTime = std::chrono::system_clock::now();
 }
 
-TimerManager::~TimerManager() 
+TimerManager::~TimerManager()
 {
+
 }
 
-std::shared_ptr<Timer> TimerManager::addTimer(uint64_t ms, std::function<void()> cb, bool recurring) 
+std::shared_ptr<Timer> TimerManager::addTimer(uint64_t ms, std::function<void()> cb, bool recurring)
 {
-    std::shared_ptr<Timer> timer(new Timer(ms, cb, recurring, this));
+    std::shared_ptr<Timer> timer(new Timer(ms, cb, recurring, this)); 
     addTimer(timer);
+
     return timer;
 }
 
@@ -123,20 +126,21 @@ std::shared_ptr<Timer> TimerManager::addConditionTimer(uint64_t ms, std::functio
 uint64_t TimerManager::getNextTimer()
 {
     std::shared_lock<std::shared_mutex> read_lock(m_mutex);
-    
+
     // reset m_tickled
     m_tickled = false;
-    
-    if (m_timers.empty())
+
+    if(m_timers.empty())
     {
         // 返回最大值
         return ~0ull;
     }
 
+    auto it = m_timers.begin();
     auto now = std::chrono::system_clock::now();
-    auto time = (*m_timers.begin())->m_next;
+    auto time = (*it)->m_next;
 
-    if(now>=time)
+    if(now >= time)
     {
         // 已经有timer超时
         return 0;
@@ -144,45 +148,46 @@ uint64_t TimerManager::getNextTimer()
     else
     {
         auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(time - now);
-        return static_cast<uint64_t>(duration.count());            
-    }  
+        return static_cast<uint64_t>(duration.count());
+    }
+
 }
 
 void TimerManager::listExpiredCb(std::vector<std::function<void()>>& cbs)
 {
     auto now = std::chrono::system_clock::now();
 
-    std::unique_lock<std::shared_mutex> write_lock(m_mutex); 
+    std::unique_lock<std::shared_mutex> write_lock(m_mutex);
 
     bool rollover = detectClockRollover();
-    
     // 回退 -> 清理所有timer || 超时 -> 清理超时timer
-    while (!m_timers.empty() && rollover || !m_timers.empty() && (*m_timers.begin())->m_next <= now)
+    while(!m_timers.empty() && rollover || !m_timers.empty() && (*m_timers.begin())->m_next < now)
     {
         std::shared_ptr<Timer> temp = *m_timers.begin();
         m_timers.erase(m_timers.begin());
-        
-        cbs.push_back(temp->m_cb); 
 
-        if (temp->m_recurring)
+        cbs.push_back(temp->m_cb);
+
+        if(temp->m_recurring)
         {
-            // 重新加入时间堆
             temp->m_next = now + std::chrono::milliseconds(temp->m_ms);
             m_timers.insert(temp);
         }
-        else
-        {
-            // 清理cb
+        else{
+            // 清理cb 
             temp->m_cb = nullptr;
         }
     }
 }
 
-bool TimerManager::hasTimer() 
+bool TimerManager::hasTimer()
 {
     std::shared_lock<std::shared_mutex> read_lock(m_mutex);
     return !m_timers.empty();
 }
+
+
+
 
 // lock + tickle()
 void TimerManager::addTimer(std::shared_ptr<Timer> timer)
@@ -190,27 +195,31 @@ void TimerManager::addTimer(std::shared_ptr<Timer> timer)
     bool at_front = false;
     {
         std::unique_lock<std::shared_mutex> write_lock(m_mutex);
-        auto it = m_timers.insert(timer).first;
+        auto it = m_timers.insert(timer).first; // 返回值为std::pair<iterator, bool>
         at_front = (it == m_timers.begin()) && !m_tickled;
         
-        // only tickle once till one thread wakes up and runs getNextTime()
+        // only tickle once  
+        // till one thread wakes up and runs getNextTime()
         if(at_front)
         {
             m_tickled = true;
         }
+
     }
-   
+
     if(at_front)
     {
-        // wake up 
+        // // wake up 
         onTimerInsertedAtFront();
     }
 }
+
 
 bool TimerManager::detectClockRollover() 
 {
     bool rollover = false;
     auto now = std::chrono::system_clock::now();
+    // 判断当前时间是否比（上一次时间 - 1小时）更早
     if(now < (m_previouseTime - std::chrono::milliseconds(60 * 60 * 1000))) 
     {
         rollover = true;
@@ -219,5 +228,5 @@ bool TimerManager::detectClockRollover()
     return rollover;
 }
 
-}
 
+}
